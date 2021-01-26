5c38283feb4372a54db0dd89d930ad7792c178e7
vbaobao <vbao.main@gmail.com>
2021-01-25 20:16:35 -0800

    Write boilerplate html, css, and plan app.js contents

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,16 @@
-
-
+/*
+ * FUNCTIONS NEEDED FOR GAME MECHANICS
+ * GAME/GLOBAL
+ * - constructor includes: x wins, o wins, last winner
+ * - method to start game (same as reset), start with x the first time, then most recent winner, creates new board
+ * - method that click listener attaches to, updates board based on where clicked
+ * - method to increase score on DOM
+ * - method to update board on DOM
+ *
+ * BOARD CLASS
+ * - constructor: board is array of arrays
+ * - method to update board
+ * - method to check winner
+ * - method to alert winner
+ * - method to clear board
+ * /
