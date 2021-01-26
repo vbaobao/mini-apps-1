7fd5e202ec3ad3f4e5d76ed455f7bd0bea32029b
vbaobao <vbao.main@gmail.com>
2021-01-25 20:46:46 -0800

    Write app.js base format

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,11 +1,11 @@
 /*
  * FUNCTIONS NEEDED FOR GAME MECHANICS
  * GAME/GLOBAL
- * - constructor includes: x wins, o wins, last winner
- * - method to start game (same as reset), start with x the first time, then most recent winner, creates new board
- * - method that click listener attaches to, updates board based on where clicked
+ * - global vars: x wins, o wins, last winner
+ * - method to start game (same as reset), start with x the first time
+ *   then most recent winner, creates new board
  * - method to increase score on DOM
- * - method to update board on DOM
+ * - method to update board on DOM (player click listener here)
  *
  * BOARD CLASS
  * - constructor: board is array of arrays
@@ -13,4 +13,36 @@
  * - method to check winner
  * - method to alert winner
  * - method to clear board
- * /
+ */
+
+// Global variables are not recommended, but using them for game
+// as it will reset the game score on reload
+// player object tracks how many points each player has
+var player = {
+  x: 0,
+  o: 0
+};
+var lastWinner = player.x;
+
+var startGame = () => {};
+
+var updateBoardDOM = () => {};
+
+var updateScoreDOM = () => {};
+
+class Board {
+  constructor(starter) {
+    this.board = new Array(9).fill(0);
+  }
+
+  // Board methods
+  checkWinner() {}
+
+  updateBoard(player, index) {}
+
+  alertWinner(player) {}
+
+  clearBoard() {}
+}
+
+// Initialize game below
