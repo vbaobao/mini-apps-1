ec2b1096577b14f25ee21047aa332fca97a540b6
vbaobao <vbao.main@gmail.com>
2021-01-28 13:13:47 -0800

    Connect Express with client folder

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -3,7 +3,10 @@ const app = express();
 const port = 3000;
 
 app.listen(port, () => { console.log(`Listening on port ${port}`); });
+app.use('/', express.static('client'));
 
 app.get('/', (req,res) => {
   console.log('test');
 });
+
+app.post('/', (req,res) => {});
