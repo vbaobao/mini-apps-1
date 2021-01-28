9d2aa57da43622c750e66f5e9cfff142353d57c5
vbaobao <vbao.main@gmail.com>
2021-01-28 13:36:13 -0800

    Write functional client connected to server.js directly

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -2,11 +2,12 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.listen(port, () => { console.log(`Listening on port ${port}`); });
-app.use('/', express.static('client'));
-
-app.get('/', (req,res) => {
-  console.log('test');
+app.listen(port, () => {
+  console.log(`Listening on port ${port}: http://localhost:3000`);
 });
 
-app.post('/', (req,res) => {});
+app.use('/', express.static('client'));
+
+app.post('/', (req,res) => {
+  console.log('test');
+});
