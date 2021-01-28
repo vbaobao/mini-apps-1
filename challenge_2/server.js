524d29e5de8daf0b52ffe3c085fcdd08391c8e2c
vbaobao <vbao.main@gmail.com>
2021-01-28 11:06:18 -0800

    Write package.json file and base Express

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,9 @@
+const express = require('express');
+const app = express();
+const port = 3000;
 
+app.listen(port, () => { console.log(`Listening on port ${port}`); });
 
+app.get('/', (req,res) => {
+  console.log('test');
+});
