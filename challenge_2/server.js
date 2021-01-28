039ed6e543c92a1a6fcf9a4dc66d0733634d869b
vbaobao <vbao.main@gmail.com>
2021-01-28 14:07:38 -0800

    Imported body-parser to get form req functioning

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 
@@ -6,8 +7,12 @@ app.listen(port, () => {
   console.log(`Listening on port ${port}: http://localhost:3000`);
 });
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 app.use('/', express.static('client'));
 
 app.post('/', (req,res) => {
-  console.log('test');
+  console.log(req.body);
+  console.log(req.body.name);
+  res.send(req.body.text);
 });
