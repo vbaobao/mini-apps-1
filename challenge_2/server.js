c830ef4fa06dbd9a0d2778cdf68e48154cdf0dba
vbaobao <vbao.main@gmail.com>
2021-01-28 23:04:45 -0800

    Functional JSON file parser in jsonFileParser(), pseudocode convertToCSV()

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -16,8 +16,29 @@ app.post('/', (req,res,next) => {
     if (err) { return next(err); }
     fs.readFile(files.file.path, 'utf8', (err, data) => {
       if (err) { return console.error(err); }
-      console.log(typeof data);
+      let fileObject = jsonFileParser(data);
       res.redirect('/');
     });
   });
 });
+
+var jsonFileParser = (file) => {
+  let asString = file.replace(/\r|\n|\s/g, '');
+  if (asString[asString.length - 1]) {
+    asString = asString.slice(0, asString.length - 1);
+  }
+  return JSON.parse(asString);
+};
+
+var convertToCSV = (object) => {
+  // Create an array of arrays
+  // Iterate through object keys for first array index
+  // Recursively iterate through the objects
+  // grabbing only values based on keys in array[0]
+  // push to a new array for each object
+  // recursively look through children
+
+  // Join each array of array with a comma (plain join)
+  // Join entire array with newlines
+  // return converted string
+};
