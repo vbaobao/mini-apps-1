241be01d83f62b5e455674682cd046a5d5cb2129
vbaobao <vbao.main@gmail.com>
2021-02-15 21:06:17 -0800

    Rewrite app.jsx to handle showing different forms per page.

--- challenge_3/client/lib/Form1.jsx
+++ challenge_3/client/lib/Form1.jsx
@@ -1,8 +1,12 @@
 function Form1(props) {
+  let handleNext = (e) => {
+    e.preventDefault();
+    console.log(e);
+  };
   return (
-    <div class='form1'>
+    <div>
       <h2>Create Account</h2>
-      <form>
+      <form name="form1">
         <label>
           Name:
           <input type="text" name="name" required />
@@ -15,7 +19,7 @@ function Form1(props) {
           Password:
           <input type="text" name="password" required />
         </label>
-        <input type="submit" value="Next" onClick={()=>{console.log('clicked')}} />
+        <input type="submit" value="Next" onClick={handleNext} />
       </form>
     </div>
   );
--- challenge_3/client/lib/app.jsx
+++ challenge_3/client/lib/app.jsx
@@ -1,8 +1,12 @@
 class App extends React.Component {
   constructor(props) {
     super(props)
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleNext = this.handleNext.bind(this);
+
     this.state = {
-      formNum: 1,
+      currentForm: 1,
       forms: {
         form1: {},
         form2: {},
@@ -11,13 +15,28 @@ class App extends React.Component {
     }
   }
 
-  render() { return (
-    <div>
-      <h1>Check Out</h1>
-      <Form1 />
-      <Form2 />
-      <Form3 />
-    </div>
+  handleSubmit() {}
+
+  handleNext(e) {
+    e.preventDefault();
+    //setState to form data
+  }
+
+  render() {
+    let formToRender;
+    if (this.state.currentForm === 1) {
+      formToRender = <Form1 />;
+    } else if (this.state.currentForm === 2) {
+      formToRender = <Form2 />;
+    } else if (this.state.currentForm === 3) {
+      formToRender = <Form3 />;
+    }
+
+    return (
+      <div>
+        <h1>Check Out</h1>
+        {formToRender}
+      </div>
   )}
 }
 
