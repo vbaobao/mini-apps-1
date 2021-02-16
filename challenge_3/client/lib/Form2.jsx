741185d997080abfa8d0a1de826978748e06b9c7
vbaobao <vbao.main@gmail.com>
2021-02-15 17:39:51 -0800

    Write Form 2 template

--- challenge_3/client/lib/Form2.jsx
+++ challenge_3/client/lib/Form2.jsx
@@ -1,3 +1,34 @@
 function Form2(props) {
-  return <h2>FORM2</h2>;
+  return (
+    <div class='form2'>
+      <h2>Shipping Address</h2>
+      <form>
+        <label>
+          Address Line 1:
+          <input type='text' name='address_ln1' />
+        </label>
+        <label>
+          Address Line 2:
+          <input type='text' name='address_ln2' />
+        </label>
+        <label>
+          City:
+          <input type='text' name='city' />
+        </label>
+        <label>
+          State:
+          <input type='text' name='state' />
+        </label>
+        <label>
+          Zip Code:
+          <input type='text' name='zipcode' />
+        </label>
+        <label>
+          Phone Number:
+          <input type='text' name='phone' />
+        </label>
+        <input type='submit' value='submit' onClick={()=>{console.log('clicked')}} />
+      </form>
+    </div>
+  );
 }
