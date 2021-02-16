de7224bb07227296e699a10fbc338b785be6b3d6
vbaobao <vbao.main@gmail.com>
2021-02-15 22:16:13 -0800

    Remove console.log

--- challenge_3/client/lib/Form3.jsx
+++ challenge_3/client/lib/Form3.jsx
@@ -16,7 +16,6 @@ class Form3 extends React.Component {
     let newState = {};
     newState[e.target.name] = e.target.value;
     this.setState(newState);
-    console.log(newState);
   }
 
   render() {
