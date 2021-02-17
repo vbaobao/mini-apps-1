71aedd3a5f10f1dcc352e699a8d18e5168abd55c
vbaobao <vbao.main@gmail.com>
2021-02-16 20:56:50 -0800

    Set up server port, webpack config, app.jsx boilerplate

--- challenge_4/client/app.jsx
+++ challenge_4/client/app.jsx
@@ -1,2 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
+  render() {
+    return <h1>HELLO</h1>;
+  };
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
--- challenge_4/server.js
+++ challenge_4/server.js
@@ -1,2 +1,10 @@
+const express = require('express');
+const app = express();
+const bodyparser = require('body-parser');
+const port = 3000;
 
+app.use(bodyparser.urlencoded({extended: false}));
+app.use(bodyparser.json());
+app.use(express.static());
 
+app.listen(port, console.log(`Listening on port ${port}`));
