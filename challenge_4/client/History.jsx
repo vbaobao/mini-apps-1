4f3b4a621e5a7812df1065159654ad64dc687965
vbaobao <vbao.main@gmail.com>
2021-02-19 22:11:45 -0800

    Updated component to handle unnamed game session

--- challenge_4/client/History.jsx
+++ challenge_4/client/History.jsx
@@ -4,7 +4,7 @@ function History(props) {
   let buttons = props.history.length !== 0
     ? props.history.map((game) => {
       return (
-        <button key={game.id} gameid={game.id} onClick={props.handleClick}>{game.name}</button>);
+        <button key={game.id} gameid={game.id} onClick={props.handleClick}>{game.name || 'Unnamed Game'}</button>);
     })
     : <p>No games history</p>;
   return (
